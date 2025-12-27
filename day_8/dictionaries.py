4f415a1f3020907972fa5fb0bc0ceab0894a5bd2
amceljhb025 <amceljhb025@student.wethinkcode.co.za>
2025-12-27 10:10:04 +0200

    day 07 done

---
+++ day_8/dictionaries.py
@@ -0,0 +1,17 @@
+#Exercises: Day 8
+
+#1. and #2.
+dog_dict = {"name":"Zoe","colour":"brown", "breed":"Africunus","legs":4}
+
+#3. 
+
+student_dict= {"first_name":"Amahle","last_name":"Cele","gender":"male","age":24,"marital_status":"single","skills":["Dope shit","Art","Writing","Music"],"country":"mzansi","city":"pinetown","address":"236 mfeme road"}
+
+#4.
+
+print("length of student dictionary:",len(student_dict))
+
+#5.
+
+print(type(student_dict["skills"]))
+
